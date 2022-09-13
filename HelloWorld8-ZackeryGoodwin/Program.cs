fcd023d5f9bde9e39d6d5989a3c15295a544be3d
N1sgamm <N1sgamm@gmail.com>
2022-09-13 09:12:59 -0300

    Initial commit

---
+++ HelloWorld8-ZackeryGoodwin/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld8_ZackeryGoodwin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 8")
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HelloWorld8-ZackeryGoodwin
+ 
